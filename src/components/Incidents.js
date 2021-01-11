b326cac4b024f2b4331ed4f749fc4c8f7e05b95c
Bui Nam <nambk@bravebits.vn>
2021-01-11 14:09:01 +0700

    add incident details

--- src/components/Incidents.js
+++ src/components/Incidents.js
@@ -1,8 +1,10 @@
 import React, {useContext, useState} from 'react';
 import {StatusContext} from '../contexts/AppContexts';
-import { Card, Modal, Button } from 'antd';
+import { Card, Modal, Button, Collapse } from 'antd';
+import { CaretRightOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
 
+const { Panel } = Collapse;
 export default function Incidents() {
     const {incident} = useContext(StatusContext);
 
@@ -30,12 +32,28 @@ export default function Incidents() {
                 style={{ width: 250, }}
             >
                 <h4 style={{color: '#282C70'}}>{incident.title}</h4>
-                <p>{incident.summary}</p>
+                <p><strong>Resolved - </strong>{incident.discription.resolved}</p>
                 <Button type="link" onClick={showModal} style={{padding: 0}}>
                      Incident Details
                 </Button>
                 <Modal title={incident.title} visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} width={1000}>
-                    <p>{incident.discription}</p>
+                    <Collapse
+                        bordered={false}
+                        defaultActiveKey={['1']}
+                        expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
+                        ghost
+                    >
+                        <Panel header={<h3>Resolved</h3>} key="1">
+                        <p>{incident.discription.resolved}</p>
+                        </Panel>
+                        <Panel header={<h3>Monitoring</h3>} key="2">
+                        <p>{incident.discription.monitoring}</p>
+                        </Panel>
+                        <Panel header={<h3>Identified</h3>} key="3">
+                        <p>{incident.discription.identified}</p>
+                        </Panel>
+                        
+                    </Collapse>
                 </Modal>
             </Card>
         </IncidentsWrapper>
