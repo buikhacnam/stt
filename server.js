b1ed353ebd0cdba33c0e38570e7f11af57232fe0
Bui Nam <nambk@bravebits.vn>
2021-01-20 14:05:55 +0700

    test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+  res.render('client/public');
 })
 
 app.get('/api/hello', (req, res) => {
